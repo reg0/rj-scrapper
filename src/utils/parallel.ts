43a65f1fd341afeee54bf4a5bfd119f9922a934a
Kamil Mikolajczyk <rego@gazeta.pl>
2020-03-05 22:51:26 +0100

    added ParallelTasks class

---
+++ src/utils/parallel.ts
@@ -0,0 +1,31 @@
+import { of, Observable, from } from 'rxjs';
+import { mergeMap, catchError, toArray } from 'rxjs/operators';
+
+export interface Task<T,R> {
+  input: T;
+  getTask: (input: T) => Observable<R>;
+}
+
+export interface TaskResult<T, R> {
+  input: T;
+  result: R | null;
+  error?: any;
+}
+
+export class ParallelTasks<T,R> {
+  constructor(private tasks: Task<T,R>[]) { }
+
+  doIt(parallelCount = 2): Observable<TaskResult<T,R>[]> {
+    return from(this.tasks).pipe(
+      mergeMap(
+        task => task.getTask(task.input).pipe(
+          catchError(e => of(new Error(e)))
+        ), 
+        (t, r) => (r instanceof Error ? {input: t.input, result: null, error: r} : {input: t.input, result: r}), 
+        parallelCount
+      ),
+      toArray()
+    );
+  }
+}
+
