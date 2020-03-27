1453adeb171051aa96d90e9c0b18283aa990230b
Kamil Mikolajczyk <rego@gazeta.pl>
2020-03-27 19:20:32 +0100

    removed jest and nyc

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'jest-puppeteer',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-	transform: {
-		"^.+\\.ts?$": "ts-jest"
-	},
-};
